3b2aef48d73ac1d621a2039dcc118f08842c1f64
Juan Trujillo <juantrujillobrinas@gmail.com>
2023-02-20 20:40:14 +0100

    Validaciones y mostrando presupuesto

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
 const formulario = document.querySelector("#formulario");
 const gastolistado = document.querySelector("#lista-gastos ul");
 
+//Eventos
+eventListeners();
+function eventListeners() {
+  formulario.addEventListener("submit", agregarGasto);
+}
+
 //Clases
 class Presupuesto {
   constructor(presupuesto) {
@@ -11,7 +17,9 @@ class Presupuesto {
   }
 }
 
+//Clase de interfaz maneja todo lo relacionado al HTML
 class UI {
+  //Inserta el presupuesto cuando el usuario agrega una cantidad
   insertarPresupuesto(cantidad) {
     //Extraer el valor
     const { presupuesto, restante } = cantidad;
@@ -19,6 +27,22 @@ class UI {
     //Añadir al HTML
     document.querySelector("#total").textContent = presupuesto;
     document.querySelector("#restante").textContent = restante;
+  }
+
+  imprimirAlerta(mensaje, tipo) {
+    //Crear el div
+    const divMensaje = document.createElement("div");
+
+    if (tipo === "error") {
+      divMensaje.classList.add("error");
+    } else {
+      divMensaje.classList.add("correcto");
+    }
+    //Mensaje de error
+    divMensaje.textContent = mensaje;
+
+    //Insertar en el HTML
+    document.querySelector(".contenido").insertBefore(divMensaje, formulario);
   }
 }
 
@@ -43,4 +67,17 @@ function agregarPresupuesto() {
 
   //Inyectar en el HTML
   ui.insertarPresupuesto(presupuesto);
+}
+
+function agregarGasto(e) {
+  e.preventDefault();
+  //Leer los datos del formulario
+  const nombre = document.querySelector("#gasto-usuario").value;
+  const cantidad = document.querySelector("#cantidad-usuario").value;
+
+  //Validacion de datos
+  if (nombre === "" || cantidad === "") {
+    ui.imprimirAlerta("Ambos campos son obligatorios", "error");
+    return;
+  }
 }
